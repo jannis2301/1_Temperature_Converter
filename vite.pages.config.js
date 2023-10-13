88626a465623dea06d769f37ebf16aa6bc42f32a
jannis2301 <jannis.ax@gmx.de>
2023-10-13 19:11:12 +0200

    chore: updated outDir to gh-pages

--- vite.pages.config.js
+++ vite.pages.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '',
   build: {
-    outDir: 'github-page',
+    outDir: 'gh-pages',
     rollupOptions: {
       input: './index.html',
     },
